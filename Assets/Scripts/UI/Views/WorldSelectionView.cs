e5104eeda782b12d33d019ccdf08a570e1c9e7e4
dinooo5252 <firedragon1225@naver.com>
2026-01-09 22:49:27 +0900

    [Fix] 변수 이름 변경
    snapTime 변수이름 변경 및 주석 추가

--- Assets/Scripts/UI/Views/WorldSelectionView.cs
+++ Assets/Scripts/UI/Views/WorldSelectionView.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 
 public class WorldSelectionView : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    //월드 개수 상수
     const int WORLD_COUNT = 4;
 
     public ScrollRect scrollRect;
@@ -13,9 +14,12 @@ public class WorldSelectionView : MonoBehaviour, IDragHandler, IEndDragHandler
     public RectTransform slotTrans;
     public HorizontalLayoutGroup hlg;
 
-    public float snapSpeed = 2f;
-    float snapTime = 0;
+    public float snapTime = 1f;
+    float snapTimeBase = 0;
+
+    //현재 선택된 world 번호(0~)
     int worldNum = 0;
+
     bool isSnapping;
     Vector3 targetPos = Vector3.zero;
 
@@ -28,9 +32,11 @@ public class WorldSelectionView : MonoBehaviour, IDragHandler, IEndDragHandler
     {
         if (isSnapping)
         {
-            snapTime += Time.deltaTime;
-            float t = snapTime * snapSpeed;
+            //목표 위치로 이동
+            snapTimeBase += Time.deltaTime;
+            float t = snapTimeBase / snapTime;
             contentTrans.localPosition = Vector3.Lerp(contentTrans.localPosition, targetPos, t);
+            //(snapTime)초 후 목표위치에 고정
             if (t >= 1f)
             {
                 scrollRect.velocity = Vector2.zero;
@@ -44,6 +50,8 @@ public class WorldSelectionView : MonoBehaviour, IDragHandler, IEndDragHandler
     {
         isSnapping = false;
         scrollRect.velocity = Vector2.zero;
+
+        //드래그한 위치에 따라 worldNum 계산
         worldNum = Mathf.RoundToInt(-contentTrans.localPosition.x / (hlg.spacing + slotTrans.rect.width));
         if(worldNum < 0) worldNum = 0;
         if(worldNum > WORLD_COUNT-1) worldNum = WORLD_COUNT-1;
@@ -52,8 +60,9 @@ public class WorldSelectionView : MonoBehaviour, IDragHandler, IEndDragHandler
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        //worldNum에 해당하는 월드의 위치로  목표위치 설정
         targetPos = new Vector3(-(worldNum*(hlg.spacing + slotTrans.rect.width)), contentTrans.localPosition.y, contentTrans.localPosition.z);
-        snapTime = 0;
+        snapTimeBase = 0;
         isSnapping = true;
     }
 }
