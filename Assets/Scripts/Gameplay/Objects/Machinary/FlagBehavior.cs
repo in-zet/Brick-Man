98d8765a86ff1599fdb716b064becdb41277d777
ParkYurim <t01095707899@gmail.com>
2026-01-18 16:31:41 +0900

    [Update] flag_enable, flag_disable 적용
    flag_disable에서 flag 스크립트 제거. flag스크립트에 flag_disable파괴 & flag_enable생성 함수 추가

--- Assets/Scripts/Gameplay/Objects/Machinary/FlagBehavior.cs
+++ Assets/Scripts/Gameplay/Objects/Machinary/FlagBehavior.cs
@@ -1,17 +1,20 @@
-using UnityEditor.UI;
-using UnityEngine;
+using System.Collections;
 using static Constants;
+using Unity.Cinemachine;
+using UnityEngine;
 
 public class FlagBehavior : MonoBehaviour
 {
     Collider2D flagCollider;
-    
+
+    public GameObject flag_enablePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         flagCollider = gameObject.GetComponent<Collider2D>();
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(PLAYER_TAG))
@@ -20,6 +23,10 @@ public class FlagBehavior : MonoBehaviour
 
             LevelManager.Instance.SetSpawnPoint(transform.position);
             flagCollider.enabled = false;
+            Destroy(gameObject);
+
+            // flag_enable로 교체
+            Instantiate(flag_enablePrefab, transform.position, Quaternion.identity);
         }
     }
 }
